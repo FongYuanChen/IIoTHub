a21b4927af5b14fb769b30850d4fef59a480fe0f
denny <s455358@gmail.com>
2026-01-30 09:55:13 +0800

    feat: 新增 DeviceDriverMetadataApi

---
+++ IIoTHub/IIoTHub.Api/DeviceDriverMetadataApi.cs
@@ -0,0 +1,52 @@
+﻿using IIoTHub.Application.Interfaces;
+using IIoTHub.Domain.Enums;
+using IIoTHub.Domain.Models.DeviceSettings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IIoTHub.Api
+{
+    /// <summary>
+    /// 設備驅動器資料相關接口
+    /// </summary>
+    [Route("api/deviceDriverMetadata")]
+    [ApiController]
+    public class DeviceDriverMetadataApi : ControllerBase
+    {
+        private readonly IDeviceDriverMetadataProvider _deviceDriverMetadataProvider;
+
+        public DeviceDriverMetadataApi(IDeviceDriverMetadataProvider deviceDriverMetadataProvider)
+        {
+            _deviceDriverMetadataProvider = deviceDriverMetadataProvider;
+        }
+
+        /// <summary>
+        /// 取得機台驅動器資料列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("machine")]
+        public IEnumerable<DeviceDriverSetting> GetMachineDriverMetadata()
+        {
+            return _deviceDriverMetadataProvider.GetDriverMetadata(DeviceCategoryType.Machine);
+        }
+
+        /// <summary>
+        /// 取得料倉驅動器資料列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("magazine")]
+        public IEnumerable<DeviceDriverSetting> GetMagazineDriverMetadata()
+        {
+            return _deviceDriverMetadataProvider.GetDriverMetadata(DeviceCategoryType.Magazine);
+        }
+
+        /// <summary>
+        /// 取得機台驅動器資料列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("robot")]
+        public IEnumerable<DeviceDriverSetting> GetRobotDriverMetadata()
+        {
+            return _deviceDriverMetadataProvider.GetDriverMetadata(DeviceCategoryType.Robot);
+        }
+    }
+}
